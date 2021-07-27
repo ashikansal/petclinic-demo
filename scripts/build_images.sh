3192d89032a37c14133ceef9233198463bcf0afe
Ashi Kansal <ashikansal@Ashis-MacBook-Pro.local>
2021-07-27 12:06:40 +0530

    Modified package image for rest app

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -10,6 +10,7 @@ packer build -var "resource_group=$resource_group" -var "image_name=$frontend_ap
 
 packer build -var "resource_group=$resource_group" -var "image_name=$database_app_image_name" --force ../resources/db/petclinic-db-packer-template.json
 
+rm -rf spring-petclinic-rest
 git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
 cp -r ../resources/rest/application.properties spring-petclinic-rest/src/main/resources/application.properties
 cp -r ../resources/rest/application-postgresql.properties spring-petclinic-rest/src/main/resources/application-postgresql.properties
@@ -19,5 +20,3 @@ cp -r spring-petclinic-rest/target/spring-petclinic-rest-2.4.2.jar ../resources/
 
 packer build -var "resource_group=$resource_group" -var "image_name=$rest_app_image_name" --force ../resources/rest/petclinic-rest-packer-template.json
 rm -rf spring-petclinic-rest
-
-
